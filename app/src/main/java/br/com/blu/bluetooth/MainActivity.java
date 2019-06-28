76cea02ae8073f888940e31f356eeffa75442291
Igor Araújo <igor.araujo@squadra.com.br>
2019-06-28 18:14:04 -0300

    initialize

---
+++ app/src/main/java/br/com/blu/bluetooth/MainActivity.java
@@ -0,0 +1,85 @@
+package br.com.blu.bluetooth;
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static int ENABLE_BLUETOOTH = 1;
+    public static int SELECT_PAIRED_DEVICE = 2;
+    public static int SELECT_DISCOVERED_DEVICE = 3;
+
+    static TextView statusMessage;
+    static TextView textSpace;
+    ConnectionThread connect;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        statusMessage = (TextView) findViewById(R.id.statusMessage);
+        textSpace = (TextView) findViewById(R.id.textSpace);
+
+        BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (btAdapter == null) {
+            statusMessage.setText("Que pena! Hardware Bluetooth não está funcionando :(");
+        } else {
+            statusMessage.setText("Ótimo! Hardware Bluetooth está funcionando :)");
+            if(!btAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, ENABLE_BLUETOOTH);
+                statusMessage.setText("Solicitando ativação do Bluetooth...");
+
+            } else {
+                statusMessage.setText("Bluetooth já ativado :)");
+            }
+        }
+
+        //btAdapter.enable(); ligar o bluethooth automaticamente
+        //btAdapter.disable(); desligar o bluethooth automaticamente
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main_bluetooth, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if(resultCode == RESULT_OK) {
+            statusMessage.setText("Você selecionou " + data.getStringExtra("btDevName") + "\n"
+                    + data.getStringExtra("btDevAddress"));
+
+            connect = new ConnectionThread(data.getStringExtra("btDevAddress"));
+            connect.start();
+        }
+        else {
+            statusMessage.setText("Nenhum dispositivo selecionado :(");
+        }
+    }
+}
